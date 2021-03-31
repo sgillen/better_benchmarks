3cccefeffd5b35025576b2ad3fa7bb3d09b70afa
sgillen <sgillen@ucsb.edu>
2021-03-30 17:05:11 -0700

    ppo with postprocessor

--- run_rllib.py
+++ run_rllib.py
@@ -1,34 +1,37 @@
+import ray
 from ray import tune
-from ray.rllib.agents.ppo import PPOTrainer
-from ray.rllib.agents.ars import ARSTrainer
-from ray.rllib.agents.a3c import A2CTrainer
+from ray.rllib.agents.trainer_template import build_trainer
 
-import gym
-import pybullet_envs
-from ray.tune.registry import register_env
+from ray.rllib.agents.ppo.ppo import DEFAULT_CONFIG as PPO_DEFAULT_CONFIG
+from ray.rllib.agents.ppo.ppo import validate_config as ppo_validate_config
+from ray.rllib.agents.ppo.ppo import execution_plan as ppo_execution_plan
+from ray.rllib.agents.ppo.ppo_torch_policy import PPOTorchPolicy
 
-def bullet_walker_creator(env_config):
-    import pybullet_envs
+# from ray.rllib.agents.ars import ARSTrainer
+# from ray.rllib.agents.a3c import A2CTrainer
 
-    return gym.make("Walker2DBulletEnv-v0")
+import seagul.envs
+from seagul.mesh import mdim_div_stable
 
-def bullet_cheetah_creator(env_config):
-    import pybullet_envs
 
-    return gym.make("HalfCheetahBulletEnv-v0")
+class PPOFracPolicy(PPOTorchPolicy):
+    def postprocess_trajectory(self, sample_batch, other_agent_batches=None, episode=None):
+        sample_batch[sample_batch.REWARDS] = mdim_div_stable(sample_batch[sample_batch.OBS], sample_batch[sample_batch.ACTIONS], sample_batch[sample_batch.REWARDS])
+        return super().postprocess_trajectory(sample_batch, other_agent_batches, episode)
 
-def bullet_hopper_creator(env_config):
-    import pybullet_envs
 
-    return gym.make("HopperBulletEnv-v0")
+def get_ppo_policy_class(config):
+    return PPOFracPolicy
 
-register_env("Walker2DBulletEnv-v0", bullet_walker_creator)
-register_env("HalfCheetahBulletEnv-v0", bullet_cheetah_creator)
-register_env("HopperBulletEnv-v0", bullet_hopper_creator)
 
+PPOFracTrainer = build_trainer(
+    name="PPOFrac",
+    default_config=PPO_DEFAULT_CONFIG,
+    default_policy=None,
+    get_policy_class=get_ppo_policy_class,
+)
 
 env_names = tune.grid_search(["Walker2DBulletEnv-v0", "HopperBulletEnv-v0", "HalfCheetahBulletEnv-v0"])
-on_policy_trainers = [PPOTrainer, ARSTrainer, A2CTrainer]
+on_policy_trainers = [PPOFracTrainer]#, ARSTrainer, A2CTrainer]
 
-
-tune.run(on_policy_trainers, config={"env": env_names, "batch_mode":"complete_episodes"}, checkpoint_at_end=True)
+tune.run(on_policy_trainers, config={"env": env_names, "batch_mode": "complete_episodes", "framework": "torch"}, checkpoint_at_end=True)
